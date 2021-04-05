c30e2f8b13169dfaf6711dff04126ba4de91fa77
Vibronic <andrey.brester@yandex.ru>
2021-04-05 10:20:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Proizvoditel_potrebitel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scene = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.scene)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scene
+            // 
+            this.scene.Location = new System.Drawing.Point(12, 12);
+            this.scene.Name = "scene";
+            this.scene.Size = new System.Drawing.Size(1880, 1017);
+            this.scene.TabIndex = 0;
+            this.scene.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(33, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Производители";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1491, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Потребители";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(840, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Склад";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(840, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 37);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 637);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1275, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1235, 737);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 15);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(840, 797);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(371, 203);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Конец";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scene);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.scene)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox scene;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+//Брестер.А.Н БББО-07-19
+namespace Proizvoditel_potrebitel
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public Point position(Point Tochka,int pos)
+        {
+            Point vivod = new Point();
+            if (Tochka.X > 850)
+            {
+                vivod.X = 850 + (((Tochka.X  - (850 )) / 4) * pos);
+            }else if(Tochka.X < 850)
+            {
+                vivod.X = 700 - ((((850) - (Tochka.X )) / 4) * pos);
+            }
+            if (Tochka.Y < 450)
+            {
+                vivod.Y = 450 - ((((450 ) - (Tochka.Y)) / 4) * pos);
+            }
+            else if (Tochka.Y > 450)
+            {
+                vivod.Y = 450+  (((Tochka.Y  - (450 )) / 4) * pos);
+            }
+            return vivod ;
+        }
+        static bool[,] dostavka = new bool[,] { {false, false, false, false, false },{ false, false, false, false, false },{ false, false, false, false, false } };
+        public static Image[] model = new Image[] { Image.FromFile("../../resources/Sclad.png"), Image.FromFile("../../resources/Potrebitel.png"), Image.FromFile("../../resources/Proizvoditel.png"), Image.FromFile("../../resources/Posilka.png") };
+        static Bitmap Scene = new Bitmap(1800,1000);
+        static Graphics g = Graphics.FromImage(Scene);
+        static string[] labele = new string[] {"sleep", "sleep", "sleep", "sleep", "sleep"};
+        private static void dobavlenie(int n,int Pos)
+        {
+            if (Pos == 0) { labele[0] = n.ToString(); }
+            if (Pos == 1) { labele[1] = n.ToString(); }
+            if (Pos == 2) { labele[2] = n.ToString(); }
+           
+            
+            dostavka[2, Pos] = true;
+            Thread.Sleep(2000);
+            dostavka[2, Pos] = false;
+            dostavka[1, Pos] = true;
+            Thread.Sleep(2000);
+            dostavka[1, Pos] = false;
+            dostavka[0, Pos] = true;
+            Thread.Sleep(2000);
+            dostavka[0, Pos] = false;
+            ochered += n;
+        }
+        private static void Umenshenie(int n,int Pos)
+        {
+            if (Pos == 3) { labele[3] = n.ToString(); }
+            if (Pos == 4) { labele[4] = n.ToString(); }
+            ochered -= n;
+            dostavka[0, Pos] = true;
+            Thread.Sleep(2000);
+            dostavka[0, Pos] = false;
+            dostavka[1, Pos] = true;
+            Thread.Sleep(2000);
+            dostavka[1, Pos] = false;
+            dostavka[2, Pos] = true;
+            Thread.Sleep(2000);
+            dostavka[2, Pos] = false;
+           
+        }
+        private static bool end = false;
+        public static int ochered = 0;
+        public static void proizvoditel(int N)
+        {
+            
+            Random rand=new Random();
+            bool sleep = false;
+            while (end == false)
+            {
+                Thread.Sleep(100);
+                if (ochered < 80)
+                {
+                    sleep = false;
+                }
+                else
+                {
+                    if (ochered > 100)
+                    {
+                        sleep = true;
+                        labele[N] = "sleep";
+                    }
+                }
+                if (!sleep)
+                {
+                   dobavlenie(rand.Next(1, 100),N); 
+                }
+            }
+        }
+        public static void potrebitel(int Pos)
+        {
+                Random rand = new Random();
+                while ((end == false) || (ochered != 0))
+                {
+                    if (ochered > 0)
+                    {
+                    if (ochered > 100) { Umenshenie(rand.Next(1, 100), Pos); } else { if (ochered > 1) { Umenshenie(rand.Next(1, ochered), Pos); }else { Umenshenie(1, Pos); } }; 
+                    }
+                }
+        }
+        public static void otrisovka_posilki()
+        {
+            
+        }
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            g.DrawImage(model[2], 10, 75, 128, 128);
+            g.DrawImage(model[2], 10, 350, 128, 128);
+            g.DrawImage(model[2], 10, 650, 128, 128);
+
+            g.DrawImage(model[0], new Point(600, 200));
+            g.DrawImage(model[1], new Point(1275, 550));
+            g.DrawImage(model[1], new Point(1275, 100));
+            
+                Thread Proizv1, Proizv2, Proizv3;
+            Proizv1 = new Thread(() => proizvoditel(0));
+            Proizv2 = new Thread(() => proizvoditel(1));
+            Proizv3 = new Thread(() => proizvoditel(2));
+            Proizv1.Start();
+            Proizv2.Start();
+            Proizv3.Start();
+            Thread Potreb1, Potreb2;
+            Potreb1=new Thread(() => potrebitel(3));
+            Potreb2=new Thread(() => potrebitel(4));
+            Potreb1.Start();
+            Potreb2.Start();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = labele[0].ToString();
+            label7.Text = labele[1].ToString();
+            label6.Text = labele[2].ToString();
+            label8.Text = labele[3].ToString();
+            label9.Text = labele[4].ToString();
+            Bitmap Scene1 =new Bitmap(Scene);
+            Graphics g1 = Graphics.FromImage(Scene1);
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if (dostavka[j, i])
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                g1.DrawImage(model[3], position(new Point(10, 75),j));
+                                break; 
+                            case 1:
+                                g1.DrawImage(model[3], position(new Point(10, 350),j));
+                                break;
+                            case 2:
+                                g1.DrawImage(model[3], position(new Point(10, 650),j));
+                                break;
+                            case 3:
+                                g1.DrawImage(model[3], position(new Point(1475, 100),j));
+                                break;
+                            case 4:
+                                g1.DrawImage(model[3], position(new Point(1475, 550),j));
+                                break;
+                        }
+                    }
+                }
+            }
+            if (ochered < 0) { ochered = 0; }
+            if (ochered > 200) { ochered = 200; }
+            label4.Text = ochered.ToString();
+            scene.Image = Scene1;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            end = true;
+            labele[0] = "sleep";
+            labele[1] = "sleep";
+            labele[2] = "sleep";
+        }
+    }
+}
